3d331d611805d61045709f3b292b6af5df6a7d27
paulcisek <paul.cisek@outlook.com>
2018-03-04 15:17:40 +0100

    013

---
+++ 013_xss/01_text-template-no-escaping/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+type page struct {
+	Title   string
+	Heading string
+	Input   string
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	home := page{
+		Title:   "Nothing Escaped",
+		Heading: "Nothing is escaped with text/html",
+		Input:   `<script>alert("You!");</script>`,
+	}
+
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", home)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
